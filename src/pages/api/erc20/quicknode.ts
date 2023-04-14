f7fc7ce16360fa92b5b2b62a5f16d6a1ba631e04
d0x471b <0x471@protonmail.com>
2023-04-14 18:56:35 +0300

    quicknode stuff for erc20
    Signed-off-by: d0x471b <0x471@protonmail.com>

---
+++ src/pages/api/erc20/quicknode.ts
@@ -0,0 +1,47 @@
+// ! We should get this as a environment variable 
+const quicknode_endpoint = "blablaquicknode.com/12039812";
+
+// Getting the block number by transaction hash thanks to  Quicknode API ❤️ 🇯🇵
+export const getTxBlockNum = (txHash: string) => {
+  const req = fetch(quicknode_endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "method": "eth_getTransactionByHash",
+      "params": [
+        txHash
+      ],
+      "id": 1,
+      "jsonrpc": "2.0"
+    }),
+    redirect: 'follow'
+  }).then(res => res.json)
+
+  return req;
+
+}
+
+
+// Getting the current block number to compare with the blocknumber that a ERC20 contract has deployed thanks to  Quicknode API ❤️ 🇯🇵
+export const getCurrentBlockNum = (txHash: string) => {
+  const req = fetch(quicknode_endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "method": "eth_getTransactionByHash",
+      "params": [
+        txHash
+      ],
+      "id": 1,
+      "jsonrpc": "2.0"
+    }),
+    redirect: 'follow'
+  }).then(res => res.json)
+
+  return req;
+
+}
