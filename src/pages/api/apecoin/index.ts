828152e9132d086d049f48b30522ac758142e038
João Carvalho <joao.carvalho@tripleoak.pt>
2023-04-15 19:37:45 +0900

    generic proofs, flattening data for starknet contract, block number adaptation

--- src/pages/api/apecoin/index.ts
+++ src/pages/api/apecoin/index.ts
@@ -1,106 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { proofOfOG } from '../erc20/proofs'
-import { ethGetProof } from '../erc20/quicknode'
-import { Contract, Provider, number } from "starknet";
-import { Data } from "../utils/data";
-import { BigNumber, utils } from "ethers";
+import { herodotusProof, proofOfOG, starknetVerify } from '../erc20/proofs'
+import { getCurrentBlockNum } from '../erc20/quicknode'
 
 type ReturnData = {
   coupons: any
 }
 
-const contract_data = {
+let contract_data = {
   address: '0x328507DC29C95c170B56a1b3A758eB7a9E73455c',
   creation_tx: '0xc2349c284ec8ec5cd8d735bb971583ef49d6d04d5048cd34adf994ce326642fd',
+  genesisBlock: -1,
   storage_slot_balance: 0
 }
+const address = '0xC29dC373AEbC2db141Aa5A8EF109CEe746a99ab8'
 
 const post = async (_req: NextApiRequest, res: NextApiResponse<ReturnData>) => {
   //const address = req.query.addr as string
-  const address = '0x466dd6e6f3f5dd8b1da504a893261fa61d16a0ed'
-  const proofOG = await proofOfOG(address, contract_data.address, contract_data.creation_tx, contract_data.storage_slot_balance)
-  const ethProof = await ethGetProof(contract_data.address, [proofOG?.slot], proofOG?.blockNum)
-  console.log(ethProof)
-
-  const rawProof = ethProof[0].proof;
-  const proof = rawProof.map((leaf: any) => Data.fromHex(leaf).toInts());
-  console.log(proof);
-
-  const flatProofByteLengths: number[] = [];
-  const flatProofWordLengths: number[] = [];
-  let flatProofValues: BigNumber[] = [];
-
-  for (const element of proof) {
-    flatProofByteLengths.push(element.sizeBytes);
-    flatProofWordLengths.push(element.values.length);
-    flatProofValues = flatProofValues.concat(element.values);
-    console.log(element.values);
+  if (contract_data.genesisBlock == -1) {
+    contract_data.genesisBlock = await getCurrentBlockNum()
   }
+  const proofOG = await proofOfOG(address, contract_data.address, contract_data.genesisBlock, contract_data.storage_slot_balance)
+  console.log(proofOG)
+  if (!proofOG) return res.status(500).json({ coupons: { "proofOfOG": false } })
 
-  const starknetProvider = new Provider({
-    sequencer: { network: "goerli-alpha" },
-  });
+  //await herodotusProof(contract_data.address, proofOG?.blockNum)
+  await starknetVerify(contract_data.address, proofOG?.slot, proofOG?.blockNum)
 
-  /*
-
-  const factsRegistry = new Contract(
-    abi,
-    process.env.HERODOTUS_FACTS_REGISTRY_ADDR!,
-    starknetProvider
-  );
-
-  const calldata = [
-    BigNumber.from(proofOG?.blockNum).toHexString(),
-    contract_data.address,
-    Data.fromHex(proofOG?.slot)
-      .toInts()
-      .values.map((value: any) => value.toHexString()),
-    flatProofByteLengths.map((length) => "0x" + length.toString(16)),
-    flatProofWordLengths.map((length) => "0x" + length.toString(16)),
-    flatProofValues.map((value) => value.toHexString()),
-  ];
-
-  await factsRegistry.call("get_storage_uint", calldata);
-  */
-
-
-  // Generate Proof from herodotus api
-  // Get contract creation block number
-  /*const WALLET_ADDRESS = _req.body.account
-  const APE_DEPLOYED_BLOCKNUM = getTxBlockNum(contract_data.creation_tx)
-  const body = {
-    originChain: "GOERLI",
-    destinationChain: "STARKNET_GOERLI",
-    blockNumber: APE_DEPLOYED_BLOCKNUM,
-    type: "ACCOUNT_ACCESS",
-    requestedProperties: {
-      ACCOUNT_ACCESS: {
-        account: address,
-        properties: [
-          "storageHash"
-        ]
-      }
-    },
-    webhookUrl: "https://webhook.site/71be6640-5bdc-4d49-909f-fed3c53b19e5"
-  }
-  const response = await fetch('https://api.herodotus.cloud?apiKey=' + env.herodotus, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(body)
-  });
-  const data = await response.json();
-  res.status(200).json(data);
-  */
+  return res.status(200).json({ coupons: { "proofOfOG": true } })
 }
 
 
 const get = async (_req: NextApiRequest, res: NextApiResponse<ReturnData>) => {
   //const address = req.query.addr as string
-  const address = '0x466dd6e6f3f5dd8b1da504a893261fa61d16a0ed'
-  const proofOG = await proofOfOG(address, contract_data.address, contract_data.creation_tx, contract_data.storage_slot_balance)
+  const blockNum = contract_data.genesisBlock == -1 ? await getCurrentBlockNum() : contract_data.genesisBlock
+  const proofOG = await proofOfOG(address, contract_data.address, blockNum, contract_data.storage_slot_balance)
   res.status(200).json({
     coupons: {
       "proofOfOG": proofOG ? true : false,
--- src/pages/api/erc20/proofs.ts
+++ src/pages/api/erc20/proofs.ts
@@ -1,28 +1,113 @@
 import { ethers } from 'ethers'
-import { ethGetStorageAt, getTxBlockNum, getTxsInBlockInterval } from './quicknode'
+import { ethGetProof, ethGetStorageAt, getTxsInBlockInterval } from './quicknode'
+import { Abi, Contract, Provider, number } from "starknet"
+import { Data } from "../utils/data"
+import { BigNumber } from "ethers"
 
-export const proofOfOG = async (address: string, contract_address: string, contract_creation_tx: string, storage_slot_balance: number) => {
-  const blocks_lookup = 10000
+export const proofOfOG = async (address: string, contract_address: string, block_number: number, storage_slot_balance: number) => {
+  /*
+  const blocks_lookup = 1000
   const creation_block = await getTxBlockNum(contract_creation_tx)
+  const creation_block = block_number
   const txs = await getTxsInBlockInterval(creation_block, creation_block + blocks_lookup, contract_address, address)
+  */
 
-  for (const tx of txs) {
-    const balance_slot_keccak = ethers.keccak256(
-      ethers.concat([
-        ethers.zeroPadValue(address, 32),
-        ethers.AbiCoder.defaultAbiCoder().encode(
-          ['uint256'],
-          [storage_slot_balance]
-        )
-      ])
-    )
+  //for (const tx of txs) {
+  const balance_slot_keccak = ethers.utils.keccak256(
+    ethers.utils.concat([
+      ethers.utils.zeroPad(address, 32),
+      ethers.utils.defaultAbiCoder.encode(
+        ['uint256'],
+        [storage_slot_balance]
+      )
+    ])
+  )
 
-    const balance = parseInt(await ethGetStorageAt(contract_address, balance_slot_keccak, tx.blockNumber), 16)
-    if (balance > 0) {
-      return { blockNum: tx.blockNumber, slot: balance_slot_keccak }
-    }
+  const balance = parseInt(await ethGetStorageAt(contract_address, balance_slot_keccak, block_number), 16)
+  if (balance > 0) {
+    //return { blockNum: tx.blockNumber, slot: balance_slot_keccak }
+    return { blockNum: block_number, slot: balance_slot_keccak }
   }
+  //}
 
   return null
 }
 
+export const herodotusProof = async (address: string, blockNum: number) => {
+  const herodotus_endpoint = process.env.HERODOTUS_API as string
+  const herodotus_api_key = process.env.HERODOTUS_API_KEY as string
+  const body = {
+    originChain: "GOERLI",
+    destinationChain: "STARKNET_GOERLI",
+    blockNumber: blockNum,
+    type: "ACCOUNT_ACCESS",
+    requestedProperties: {
+      ACCOUNT_ACCESS: {
+        account: address,
+        properties: [
+          "storageHash"
+        ]
+      }
+    },
+  }
+
+  const response = await fetch(herodotus_endpoint + '?apiKey=' + herodotus_api_key, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body)
+  });
+  const data = await response.json();
+  console.log(data)
+  return data;
+}
+
+export const starknetVerify = async (address: string, slot: string, blockNum: number) => {
+  const ethProof = await ethGetProof(address, [slot], blockNum)
+  const rawProof = ethProof.storageProof[0].proof;
+  const proof = rawProof.map((leaf: any) => Data.fromHex(leaf).toInts());
+
+  const flatProofByteLengths: number[] = [];
+  const flatProofWordLengths: number[] = [];
+  let flatProofValues: BigNumber[] = [];
+
+  for (const element of proof) {
+    flatProofByteLengths.push(element.sizeBytes);
+    flatProofWordLengths.push(element.values.length);
+    flatProofValues = flatProofValues.concat(element.values);
+  }
+
+  const starknetProvider = new Provider({
+    sequencer: { network: "goerli-alpha" },
+  });
+
+  /*
+  let abi: Abi = {
+  };
+
+  const factsRegistry = new Contract(
+    abi,
+    process.env.HERODOTUS_FACTS_REGISTRY_ADDR!,
+    starknetProvider
+  );*/
+
+  const slot_from_hex = Data.fromHex(slot)
+    .toInts()
+    .values.map((value: any) => value.toHexString())
+
+  const calldata = [
+    BigNumber.from(blockNum).toHexString(),
+    address,
+    BigNumber.from(slot_from_hex.length).toHexString(),
+    ...slot_from_hex,
+    BigNumber.from(flatProofByteLengths.length).toHexString(),
+    ...flatProofByteLengths.map((length) => "0x" + length.toString(16)),
+    BigNumber.from(flatProofWordLengths.length).toHexString(),
+    ...flatProofWordLengths.map((length) => "0x" + length.toString(16)),
+    BigNumber.from(flatProofValues.length).toHexString(),
+    ...flatProofValues.map((value) => value.toHexString()),
+  ];
+
+  //await factsRegistry.call("get_storage_uint", calldata);
+}
